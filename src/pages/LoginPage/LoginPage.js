f195d3e958afc3ce3dbbec484919f1b6ad3fef1b
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2019-06-26 12:21:01 -0500

    Update
    LoginPage, UserData

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,14 +1,95 @@
 import React from 'react';
 import './LoginPage.css';
+import IpGraphql from '../../components/conection/IpGraphql';
 
 class LoginPage extends React.Component {
 
+    state = {
+
+        CampoUsuario: " ",
+        CampoPassword: "",
+
+        data: {
+            data: {
+                signIn: null
+            },
+            errors: [{
+                message: {
+                    content: {},
+                    message: "",
+                    status: 0
+
+                }
+            }]
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    handleClick = e => {
+
+        const query = `
+            mutation {
+                signIn(user: {
+                    email: "${this.state.CampoUsuario}"
+                    password: "${this.state.CampoPassword}"
+                }) {
+                    content
+                    message
+                    status
+                } 
+            }
+        `;
+
+        const url = IpGraphql;
+        const opts = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ query })
+        };
+
+        fetch(url, opts)
+            .then(res => res.json())
+            .then(res => {
+                if (res.data.signIn != null) {
+                    console.log(res.data.signIn.content)
+                    window.localStorage.setItem('token', res.data.signIn.content)
+                    window.localStorage.setItem('email', this.state.CampoUsuario)
+                    this.setState({ data: res })
+                    console.log(res)
+                    window.location.href = '/user-data'
+                } else {
+                    alert("Error: verificar el usuario y contraseña")
+                    window.localStorage.clear()
+                }
+            })
+            .catch(console.log("DATOS INCORRECTOS"))
+        e.preventDefault()
+    }
+
     render() {
         return (
-            <div>
-                <h1>LOGIN PAGES</h1>
+            <div className="LoginPage">
+                <div className="CajaLogin">
+                    <h1 className="TituloLogin">Iniciar Sesión</h1>
+                    <div className="ContenedorCamposLogin">
+                        <label className="LabelUsuario">Usuario:</label>
+                        <input name="CampoUsuario" onChange={this.handleChange} className="InputUsuario" value={this.state.CampouUario} ></input>
+                        <br />
+                        <label className="LabelPassword">Contraseña:</label>
+                        <input name="CampoPassword" onChange={this.handleChange} className="InputPassword" value={this.state.CampoPassword}  ></input>
+                    </div>
+                    <div className="BotonesEntrarVolver">
+                        <a href="/user" onClick={this.handleClick} className="BotonEntrar">Entrar</a>
+                        <a onClick={this.handleProbar} className="BotonRegresar" href="/">Regresar</a>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
 
--- src/pages/user/UserDataPage.js
+++ src/pages/user/UserDataPage.js
@@ -1,13 +1,95 @@
 import React from 'react';
+import './UserDataPage.css';
+import ErrorPage from '../../components/Errors/ErrorPage';
+import IpGraphql from '../../components/conection/IpGraphql';
+
 
 class UserDataPage extends React.Component {
 
+    state = {
+    }
+
+    componentDidMount() {
+        console.log("DID MOUNT")
+    }
+
+    componentWillMount() {
+        const query = `
+            mutation {
+                verifyToken(jwt:{jwt: "${window.localStorage.token}"}) {
+                    content
+                    message
+                    status
+                }
+            }
+        `;
+
+        const url = IpGraphql;
+        const opts = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ query })
+        };
+
+        (fetch(url, opts)
+            .then(res => res.json())
+            .then(res => {
+                if (res.data.verifyToken != null) {
+                    this.setState({
+                        status: res.data.verifyToken.status,
+                        authorization: true,
+                    })
+                    console.log(res.data.verifyToken.status)
+                }
+            })
+            .catch(error => {
+                this.setState({ errors: error })
+            }))
+        console.log("WILL MOUNT")
+    }
+
     render() {
-        return (
-            <div>
-                <h1>USER DATA</h1>
-            </div>
-        )
+        if (this.state.authorization) {
+            return (
+                < div className="UserDataPage" >
+                    {console.log("CARGA EL TRUE")}
+                    <div className="BarraMenuLateral">
+                        <div className="MiniDatoUsuario">
+                            <img className="FotoPerfil" width="160" height="160" alt=""></img>
+                            <h2 className="NombreUsuario">Fulanito Perez</h2>
+                        </div>
+                        <div className="OpcionMenu">
+                            <h2 className="NombreMenu DatosPersonales">MODIFICAR DATOS</h2>
+                        </div>
+                        <div className="OpcionMenu">
+                            <h2 className="NombreMenu CrearListas">CREAR LISTAS</h2>
+                        </div>
+                        <div className="OpcionMenu">
+                            <h2 className="NombreMenu VerListas">VER LISTAS</h2>
+                        </div>
+                        <div className="OpcionMenu">
+                            <h2 className="NombreMenu  Lugares">LUGARES</h2>
+                        </div>
+                        <div className="OpcionMenu">
+                            <h2 className="NombreMenu EliminarCuenta">ELIMINAR CUENTA</h2>
+                        </div>
+                        <div className="OpcionMenu">
+                            <h2 className="NombreMenu CerrarSesion">SALIR</h2>
+                        </div>
+                    </div>
+                    <div className="ObjetivoMenuLateral">
+                        <h1>CAMPOS</h1>
+                    </div>
+                </div >
+            );
+        } else {
+            return (
+                <div>
+                    {console.log("CARGA EL FALSE")}
+                    <ErrorPage />
+                </div>
+            );
+        }
     }
 }
 
