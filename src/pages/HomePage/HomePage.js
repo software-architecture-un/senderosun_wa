2a414604510151b5b73e3069b869ead379be9972
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2019-06-26 06:47:51 -0500

    Update files init

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import './HomePage.css';
+import Camara from '../../images/camara.png';
+import Ruta from '../../images/rutas.png';
+import Actividad from '../../images/actividad.png';
 
 class HomePage extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>HOME PAGE</h1>
+            <div className="HomePage">
+
+                <div className="HeaderLandig">
+                    <div className="PosicionBotones">
+                        <a className="BotonIniciarSesion" href="/login"> Iniciar Sesión </a>
+                        <a className="BotonCrearCuenta" href="/register"> Crear cuenta </a>
+                    </div>
+                    <h1 className="TituloLanding"> Senderos UN </h1>
+                </div>
+
+                <div className="BodyLandig">
+                    <div className="CajaGaleria">
+                        <h2 className="SubTitulos">Galeria De Fotos</h2>
+                        <img src={Camara} width="250" height="250" alt="Imagen_Camara"></img>
+                        <div className="TextoJustificado">
+                            <p>En nuestra galeria podras las fotos que mas les han gustado a nuestros usuarios.</p>
+                            <p>Muestranos como ves el mundo.</p>
+                        </div>
+                    </div>
+
+                    <div className="CajaImagenes">
+                        <h2 className="SubTitulos">Rutas</h2>
+                        <img src={Ruta} width="250" height="250" alt="Imagen_Ruta" ></img>
+                        <div className="TextoJustificado">
+                            <p>Aqui encontraras las rutas preferidas por los usarios.</p>
+                            <p>Camina, conduce y descubre los caminos que el mundo tiene preparado para ti-</p>
+                        </div>
+
+                    </div>
+
+                    <div className="CajaActividades">
+                        <h2 className="SubTitulos">Actividades</h2>
+                        <img className="ImagenActividades" src={Actividad} width="200" height="200" alt="Imagen_Ruta" ></img>
+                        <div className="TextoJustificado">
+                            <p>Comparte en familia y con amigos de miles de actividades que estan esperando por ti.</p>
+                            <p>Cambia tu rutina ven y descubre lo que tenemos preparado para ti.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="FooterLandig">
+                    <h3 className="Universidad">Universidad Nacional De Colombia</h3>
+                    <h4 className="Materia">Arquitecura De software</h4>
+                </div>
+
             </div>
         )
     }
