efbc586f04a3f3960f434b9bd9c743e339cbe04e
unknown <diagomezpa@unal.edu.co>
2019-07-17 20:51:26 -0500

    cambiando direccion de grap

--- src/components/conection/IpGraphql.js
+++ src/components/conection/IpGraphql.js
@@ -2,4 +2,4 @@
 
 // export default 'http://34.94.253.144/graphql'
 //export default 'http://35.224.121.94/graphql'
-export default 'http://34.67.182.70/graphql'
+export default 'http://35.239.15.13/graphql'
